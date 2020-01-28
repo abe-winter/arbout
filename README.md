c2c3f77574b2fec521566f4f240b34e4213b267a
Abe Winter <abe-winter@users.noreply.github.com>
2020-01-28 17:08:15 -0500

    base schema and submission form

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 	- [guyden v aetna](https://www.courtlistener.com/opinion/1315738/guyden-v-aetna-inc/)
 	- [CPR article on guyden v aetna whistleblower consequences](https://www.cpradr.org/news-publications/articles/2008-10-17-employment-second-circuit-compels-arbitration-in-sarbanes-oxley-whistleblower-case-web)
 	- [sutton steel v bell south](https://caselaw.findlaw.com/la-court-of-appeal/1207456.html) re contracts of adhesion, citing aguillard 4-factor analysis
+- [history of arbitration / FAA with some case law refs](https://www.epi.org/publication/the-arbitration-epidemic/)
---
+++ schema/base.sql
@@ -0,0 +1,28 @@
+create extension if not exists "uuid-ossp";
+
+create table cases (
+  caseid uuid primary key default uuid_generate_v4(),
+  email_hash bytea, -- using global salt so this can be looked up
+  password_salt bytea,
+  password_hash bytea,
+  counterparty text,
+  counterparty_domain text,
+  issue_category text, -- broad category of the dispute
+  issue text,
+  case_real_id_hash bytea, -- using global salt so it can be crossed in the escrow DB
+  incident_date date,
+  dispute_date date,
+  arbitration_date date,
+  sought_dollars int,
+  settlement_dollars int,
+  subjective_fair boolean,
+  subjective_inmyfavor boolean,
+  submitter_initiated boolean,
+  arbitration_agency text,
+  submitter_choose_agency boolean,
+  arbitration_state text, -- two-digit state
+  created timestamp not null default now(),
+  modified timestamp not null default now()
+);
+
+create index cases_edit_key on cases (edit_key);
